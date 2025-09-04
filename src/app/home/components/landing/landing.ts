4b604177b00a1b3c54c278681c404989f47d3ca2
Benjamin <benjamin.bats.dev@gmail.com>
2025-09-04 13:24:50 +0200

    quick changes on landing home blinker

--- src/app/home/components/landing/landing.ts
+++ src/app/home/components/landing/landing.ts
@@ -7,7 +7,4 @@ import {Component} from '@angular/core';
   styleUrl: './landing.scss'
 })
 export class HomeLanding {
-
-  // TODO
-  // ajouter un systeme pour écrire à la palce de Hi, défilement auto et en tapant le mot clef correspodnat à la catégorie dans laquelle naviguer, sur entrée, navigguer
 }
