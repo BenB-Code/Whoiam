889bd1f0daa646613e13e37313a27dca04c6e598
Benjamin <benjamin.bats.dev@gmail.com>
2025-09-12 18:04:51 +0200

    add some projects edit some styles

--- src/app/projects/components/landing/landing.ts
+++ src/app/projects/components/landing/landing.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-project-landing',
@@ -7,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './landing.scss'
 })
 export class ProjectLanding {
+  projectsList = [
+    {
+      name: 'Kaamelott Citation Extractor',
+      description: '🛠️ An XML parser to extract and structure quotes from the Kaamelott Wikiquote pages.',
+      link: {
+        name: 'Github',
+        url: 'https://github.com/BenB-Code/Kaamelott_Citation_Extractor'
+      }
+    },
+    {
+      name: 'Kaamelott Citation API',
+      description: "Une API REST qui permet de récupérer, filtrer et rechercher des citations cultes de la série Kaamelott d'Alexandre Astier.",
+      link: {
+        name: 'Github',
+        url: 'https://github.com/BenB-Code/Kaamelott_Citation_API'
+      }
+    },
+    {
+      name: 'Whoiam',
+      description: 'Quick portfolio websitte that act as a resume.',
+      link: {
+        name: 'Github',
+        url: 'https://github.com/BenB-Code/Whoiam'
+      }
+    },
+  ]
 
+  redirect(url: string): void {
+    window.open(url, '_blank');
+  }
 }
