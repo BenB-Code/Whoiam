7d1951cc6870b307d69c96e1b0c0c108c0c74754
Benjamin <benjamin.bats.dev@gmail.com>
2025-09-22 15:29:27 +0200

    changes in app sizes and code format to easily get 100% height and width without anything over tasks/app bars

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,11 @@ import {RouterOutlet} from '@angular/router';
 import {HeaderBar} from './header-bar/header-bar';
 import {AppBar} from './app-bar/app-bar';
 import {ListingWindow} from './common/listing-window/listing-window';
-import {ContentWindow} from './common/content-window/content-window';
+import {Contact} from './contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderBar, AppBar, ListingWindow, ContentWindow],
+  imports: [RouterOutlet, HeaderBar, AppBar, Contact, ListingWindow],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
