7b9df107a9f2d42aa3dc652cf769bbb550e23764
Benjamin <benjamin.bats.dev@gmail.com>
2025-09-22 15:28:34 +0200

    rework conetnt-window to integrate basic interactions and matching new sizes handling

--- src/app/common/content-window/content-window.ts
+++ src/app/common/content-window/content-window.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-content-window',
-  imports: [],
-  templateUrl: './content-window.html',
-  styleUrl: './content-window.scss'
+    selector: 'app-content-window',
+    imports: [],
+    templateUrl: './content-window.html',
+    styleUrl: './content-window.scss'
 })
 export class ContentWindow {
+    @Output() closeEvent = new EventEmitter<void>();
+    @Output() fullscreenEvent = new EventEmitter<boolean>();
+    @Output() reduceEvent = new EventEmitter<void>();
 
+    isFullscreen: boolean = false;
+
+    close() {
+        this.closeEvent.emit();
+    }
+
+    fullscreen() {
+        this.isFullscreen = !this.isFullscreen;
+        this.fullscreenEvent.emit(this.isFullscreen);
+    }
+
+    reduce() {
+        this.reduceEvent.emit();
+    }
 }
