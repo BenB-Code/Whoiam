09f9038b1f0638a011fec4923d19fea60159c88e
Benjamin <benjamin.bats.dev@gmail.com>
2025-09-24 11:03:34 +0200

    Adapt app.ts/scss to integrate new experiences component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,13 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeaderBar} from './header-bar/header-bar';
 import {AppBar} from './app-bar/app-bar';
-import {ListingWindow} from './common/listing-window/listing-window';
 import {Contact} from './contact/contact';
 import {Home} from './home/home';
+import {Experiences} from './experiences/experiences';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderBar, AppBar, Contact, Home, ListingWindow],
+  imports: [RouterOutlet, HeaderBar, AppBar, Contact, Home, Experiences],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
