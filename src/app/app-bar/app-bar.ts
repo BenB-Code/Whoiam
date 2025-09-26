7156c029fecc64ef81fcb94b1b528e7917d851b5
Benjamin <benjamin.bats.dev@gmail.com>
2025-09-26 15:34:22 +0200

    adding events on app bar icons clicks

--- src/app/app-bar/app-bar.ts
+++ src/app/app-bar/app-bar.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {map, take} from 'rxjs';
+import {openWindow, restoreWindow, selectAllWindows, setActiveWindow, WindowState, WindowType} from '../store';
+import {CLOSED, MINIMIZED} from '../store/window-manager/models/status.const';
 
 @Component({
   selector: 'app-app-bar',
@@ -7,5 +11,32 @@ import { Component } from '@angular/core';
   styleUrl: './app-bar.scss'
 })
 export class AppBar {
+  private store = inject(Store);
 
+  windowState$ = this.store.select(selectAllWindows).pipe(
+    map(windows => {
+      const stateMap: Record<string, WindowState | null> = {};
+      windows.forEach(window => {
+        stateMap[window.id] = window;
+      });
+      return stateMap
+    })
+  )
+
+  onAppClick(id: WindowType) {
+    this.store.select(selectAllWindows).pipe(
+      map(windows => windows.find(w => w.id === id)),
+      take(1)
+    ).subscribe(window => {
+        if (!window || window.status === CLOSED) {
+          this.store.dispatch(openWindow({id}));
+        } else if (window.status === MINIMIZED) {
+          this.store.dispatch(restoreWindow({id}));
+        } else {
+          this.store.dispatch(setActiveWindow({id}));
+        }
+
+      }
+    )
+  }
 }
