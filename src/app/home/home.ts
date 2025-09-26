a4a319222d960d280c5f89bd23ae6adf6be989cf
Benjamin <benjamin.bats.dev@gmail.com>
2025-09-26 15:34:46 +0200

    binding store with home component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,29 +1,38 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {ContentWindow} from '../common/content-window/content-window';
+import {Store} from '@ngrx/store';
+import {closeWindow, maximizeWindow, minimizeWindow, selectWindowById, setActiveWindow, WindowState} from '../store';
+import {Observable} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
+import {HOME} from '../store/window-manager/models/types.const';
 
 @Component({
   selector: 'app-home',
   imports: [
     ContentWindow,
-
+    AsyncPipe
   ],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
-  isFullscreen = false;
-  isReduced = false;
-  isVisible = true;
+  private store = inject(Store);
+
+  homeWindow$: Observable<WindowState | null> = this.store.select(selectWindowById(HOME));
 
   onClose(): void {
-    this.isVisible = false;
+    this.store.dispatch(closeWindow({id: HOME}));
   }
 
-  onFullscreen(isFullscreen: boolean): void {
-    this.isFullscreen = isFullscreen;
+  onFullscreen(): void {
+    this.store.dispatch(maximizeWindow({id: HOME}));
   }
 
   onReduce(): void {
-    this.isReduced = true;
+    this.store.dispatch(minimizeWindow({id: HOME}));
+  }
+
+  onActivate(): void {
+    this.store.dispatch(setActiveWindow({id: HOME}));
   }
 }
