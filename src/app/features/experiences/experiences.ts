48377a665a0cca48e37f4e82e9b890158997a1c9
Benjamin <benjamin.bats.dev@gmail.com>
2025-09-29 17:57:36 +0200

    add experiences implementation

--- src/app/features/experiences/experiences.ts
+++ src/app/features/experiences/experiences.ts
@@ -1,35 +1,32 @@
-import {Component, inject, OnInit, signal} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {ListingWindow} from '../../common/listing-window/listing-window';
 import {Experience} from './models/experience.model';
 import {Store} from '@ngrx/store';
-import {Observable, take} from 'rxjs';
+import {Observable} from 'rxjs';
 import {closeWindow, maximizeWindow, minimizeWindow, selectWindowById, setActiveWindow, WindowState} from '../../store';
 import {EXPERIENCES} from '../../store/window-manager/constants/types.const';
 import {AsyncPipe} from '@angular/common';
 import {ExperiencesService} from './services/experiences.service';
+import {Spinner} from '../../common/spinner/spinner';
 
 @Component({
   selector: 'app-experiences',
   imports: [
     ListingWindow,
-    AsyncPipe
+    AsyncPipe,
+    Spinner
   ],
   templateUrl: './experiences.html',
   styleUrl: './experiences.scss'
 })
-export class Experiences implements OnInit {
+export class Experiences {
   private store = inject(Store);
   experiencesWindow$: Observable<WindowState | null> = this.store.select(selectWindowById(EXPERIENCES));
 
-  private experiencesService: ExperiencesService = inject(ExperiencesService);
-  experiences = signal<Experience[]>([]);
+  experiencesService: ExperiencesService = inject(ExperiencesService);
 
   selectedIndex = signal<number | null>(null);
   selectedExperience = signal<Experience | null>(null);
-
-  ngOnInit(): void {
-    this.experiencesService.getExperiences().pipe(take(1)).subscribe(experiences => this.experiences.set(experiences))
-  }
 
   onSelection(event: { item: Experience, index: number }) {
     this.selectedIndex.set(event.index);
--- src/app/features/experiences/services/experiences.service.ts
+++ src/app/features/experiences/services/experiences.service.ts
@@ -1,28 +1,56 @@
-import {inject, Injectable} from '@angular/core';
+import {computed, inject, Injectable, signal} from '@angular/core';
 import {DataService} from '../../../services/data/data.service';
-import {catchError, map, Observable, of,} from 'rxjs';
+import {catchError, map, Observable, of, tap,} from 'rxjs';
 import {Experience} from '../models/experience.model';
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class ExperiencesService {
-  private dataService = inject(DataService);
+    private dataService = inject(DataService);
 
-  getExperiences(): Observable<Experience[]> {
-    return this.dataService.fetchJson<Experience[]>('/assets/data/experiences.json').pipe(
-      map((experiences: Experience[]) => (experiences.map(exp => ({
-          ...exp,
-          duration: {
-            endDate: new Date(exp.duration.endDate),
-            startDate: new Date(exp.duration.startDate)
-          }
-        })))
-      ),
-      catchError(err => {
-        console.error('Error loading experiences: ', err)
-        return of([]);
-      })
-    )
-  }
+    experiences = signal<Experience[]>([]);
+    isLoading = signal<boolean>(false);
+    error = signal<string | null>(null);
+
+    hasError = computed(() => this.error() !== null);
+    isEmpty = computed(() => !this.hasError() && this.experiences().length === 0);
+    shouldDisplayPlaceholder = computed(() => this.hasError() || this.isEmpty());
+    placeholder = computed(() => this.hasError() ? this.error() : "Aucune expérience disponible pour le moment");
+
+    constructor() {
+        this.loadExperiences();
+    }
+
+    private loadExperiences() {
+        this.getExperiences().pipe(takeUntilDestroyed()).subscribe();
+    }
+
+    private getExperiences(): Observable<Experience[]> {
+        this.isLoading.set(true);
+        this.error.set(null);
+
+        return this.dataService.fetchJson<Experience[]>('/assets/data/experiences.json').pipe(
+            map((experiences: Experience[]) => (experiences.map(exp => ({
+                    ...exp,
+                    duration: {
+                        endDate: new Date(exp.duration.endDate),
+                        startDate: new Date(exp.duration.startDate)
+                    }
+                })))
+            ),
+            tap(experiences => {
+                this.experiences.set(experiences);
+                this.isLoading.set(false);
+            }),
+            catchError(err => {
+                console.error('Error loading experiences: ', err)
+                this.error.set("Une erreur est survenue lors de la récupération des expériences. Merci de réessayer plus tard.");
+                this.isLoading.set(false);
+                this.experiences.set([]);
+                return of([]);
+            })
+        )
+    }
 }
