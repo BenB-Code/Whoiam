0ac24de60a4fdee5c0692c3e86f4910e49021e77
Benjamin <benjamin.bats.dev@gmail.com>
2025-09-29 12:58:51 +0200

    fix issue with ssr and absolute file path resolving

--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -1,14 +1,27 @@
-import {inject, Injectable} from '@angular/core';
+import {inject, Injectable, PLATFORM_ID} from '@angular/core';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
+import {DOCUMENT, isPlatformBrowser} from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  private http = inject(HttpClient)
+  private http = inject(HttpClient);
+  private platformId = inject(PLATFORM_ID);
+  private document = inject(DOCUMENT);
 
   fetchJson<T>(path: string): Observable<T> {
-    return this.http.get<T>(path);
+    const fullUrl = this.getAbsoluteUrl(path);
+    return this.http.get<T>(fullUrl);
+  }
+
+  private getAbsoluteUrl(path: string): string {
+    if (isPlatformBrowser(this.platformId)) {
+      return path;
+    } else {
+      const origin = this.document?.location?.origin || 'http://localhost:4200';
+      return `${origin}${path}`;
+    }
   }
 }
