2babf9cef0e3316edd1146abda8c8c92e78f7a20
Benjamin <benjamin.bats.dev@gmail.com>
2025-10-01 17:28:53 +0200

    Test pre-commit different cases

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,4 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-console.log('TEST de hook pre-commit');
-
 bootstrapApplication(App, appConfig).catch(err => console.error(err));
