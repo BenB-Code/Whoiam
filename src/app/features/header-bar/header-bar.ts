1f2c85bbc3698e6ec9a428e83c6bca8d4bda21c2
Benjamin <benjamin.bats.dev@gmail.com>
2025-10-13 16:10:08 +0200

    quick for dev purpose on changing language handling

--- src/app/features/header-bar/header-bar.ts
+++ src/app/features/header-bar/header-bar.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, DestroyRef, inject, signal } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { EN, FR } from '../../common/constants/lang.const';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header-bar',
@@ -10,7 +12,11 @@ import { DatePipe } from '@angular/common';
 })
 export class HeaderBar {
   readonly time = signal(new Date());
+  protected readonly EN = EN;
+  protected readonly FR = FR;
+  protected activeLang: typeof FR | typeof EN = FR;
   private readonly destroyRef = inject(DestroyRef);
+  private readonly translateService: TranslateService = inject(TranslateService);
 
   constructor() {
     const intervalId = setInterval(() => {
@@ -20,5 +26,10 @@ export class HeaderBar {
     this.destroyRef.onDestroy(() => {
       clearInterval(intervalId);
     });
+  }
+
+  setLang(lang: typeof FR | typeof EN): void {
+    this.activeLang = lang;
+    this.translateService.use(lang);
   }
 }
