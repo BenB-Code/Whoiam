e82e9895667c6c14d35c11db1ede84a4fac6e04a
Benjamin <benjamin.bats.dev@gmail.com>
2025-10-13 17:00:35 +0200

    i18n on loading alt

--- src/app/common/components/spinner/spinner.ts
+++ src/app/common/components/spinner/spinner.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-spinner',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './spinner.html',
   styleUrl: './spinner.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
