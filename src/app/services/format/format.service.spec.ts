33132ffdc8e36111a3a20559c4fd71d722ad46c1
Benjamin <benjamin.bats.dev@gmail.com>
2025-10-26 10:21:35 +0100

    format service tests

--- src/app/services/format/format.service.spec.ts
+++ src/app/services/format/format.service.spec.ts
@@ -1,16 +1,46 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FormatService } from './format.service';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { US_LOCAL } from '../../common/constants';
 
-describe('FormatService', () => {
+describe('Service - FormatService', () => {
   let service: FormatService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideZonelessChangeDetection()],
+    });
     service = TestBed.inject(FormatService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('formatDuration', () => {
+    let dates: {
+      startDate: Date;
+      endDate: Date;
+    };
+
+    beforeEach(() => {
+      dates = {
+        startDate: new Date('11/01/2024'),
+        endDate: new Date('06/01/2025'),
+      };
+    });
+
+    it('should return formated dates into shorter format without local given', () => {
+      const result = service.formatDuration(dates);
+
+      expect(result).toEqual('11/2024 - 06/2025');
+    });
+
+    it('should return formated dates into shorter format with local given', () => {
+      const result = service.formatDuration(dates, US_LOCAL);
+
+      expect(result).toEqual('11/2024 - 06/2025');
+    });
   });
 });
--- src/app/services/format/format.service.ts
+++ src/app/services/format/format.service.ts
@@ -5,9 +5,9 @@ import { FR_LOCAL } from '../../common/constants';
   providedIn: 'root',
 })
 export class FormatService {
-  formatDuration(duration: { startDate: Date; endDate: Date }): string {
-    const startMonth = duration.startDate.toLocaleDateString(FR_LOCAL, { month: '2-digit', year: 'numeric' });
-    const endMonth = duration.endDate.toLocaleDateString(FR_LOCAL, { month: '2-digit', year: 'numeric' });
+  formatDuration(duration: { startDate: Date; endDate: Date }, local = FR_LOCAL): string {
+    const startMonth = duration.startDate.toLocaleDateString(local, { month: '2-digit', year: 'numeric' });
+    const endMonth = duration.endDate.toLocaleDateString(local, { month: '2-digit', year: 'numeric' });
     return `${startMonth} - ${endMonth}`;
   }
 }
